ca9ca52626c181e7923db45474cee6a7d7315137
Jostein Hjorland Tysse <jostein.h.tysse@gmail.com>
2023-11-20 13:58:38 +0100

    Prettify code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,54 +1,50 @@
-import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
-import clsx from 'clsx';
+import React from "react";
+import { Work_Sans, Spline_Sans_Mono } from "next/font/google";
+import clsx from "clsx";
 
-import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
+import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from "@/constants";
 
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import './styles.css';
-import RespectMotionPreferences from '@/components/RespectMotionPreferences';
-import { cookies } from 'next/headers';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./styles.css";
+import RespectMotionPreferences from "@/components/RespectMotionPreferences";
+import { cookies } from "next/headers";
 
 const mainFont = Work_Sans({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family',
+  subsets: ["latin"],
+  display: "fallback",
+  weight: "variable",
+  variable: "--font-family",
 });
 const monoFont = Spline_Sans_Mono({
-  subsets: ['latin'],
-  display: 'fallback',
-  weight: 'variable',
-  variable: '--font-family-mono',
+  subsets: ["latin"],
+  display: "fallback",
+  weight: "variable",
+  variable: "--font-family-mono",
 });
 
 export const metadata = {
   title: BLOG_TITLE,
-  description: 'A wonderful blog about JavaScript',
-}
+  description: "A wonderful blog about JavaScript",
+};
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
-  const savedTheme = cookies().get('color-theme');
-  const initialTheme = savedTheme?.value || 'light';
-
+  const savedTheme = cookies().get("color-theme");
+  const initialTheme = savedTheme?.value || "light";
 
   return (
     <html
       lang="en"
       className={clsx(mainFont.variable, monoFont.variable)}
       data-color-theme={initialTheme}
-      style={initialTheme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+      style={initialTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
     >
       <body>
-      <RespectMotionPreferences>
-        <Header initialTheme={initialTheme} />
-        <main>{children}</main>
-        <Footer />
-      </RespectMotionPreferences>
+        <RespectMotionPreferences>
+          <Header initialTheme={initialTheme} />
+          <main>{children}</main>
+          <Footer />
+        </RespectMotionPreferences>
       </body>
     </html>
   );
--- src/components/DarkLightToggle/DarkLightToggle.js
+++ src/components/DarkLightToggle/DarkLightToggle.js
@@ -1,37 +1,31 @@
-'use client';
-import React from 'react';
-import { Sun, Moon } from 'react-feather';
-import Cookie from 'js-cookie';
-import VisuallyHidden from '../VisuallyHidden';
-import {LIGHT_TOKENS, DARK_TOKENS} from "@/constants"
+"use client";
+import React from "react";
+import { Sun, Moon } from "react-feather";
+import Cookie from "js-cookie";
+import VisuallyHidden from "../VisuallyHidden";
+import { LIGHT_TOKENS, DARK_TOKENS } from "@/constants";
 
 function DarkLightToggle({ initialTheme, className }) {
   const [theme, setTheme] = React.useState(initialTheme);
 
   function handleClick() {
-    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    const nextTheme = theme === "light" ? "dark" : "light";
 
     // Update the state variable.
     // This causes the Sun/Moon icon to flip.
     setTheme(nextTheme);
 
     // Write the cookie for future visits
-    Cookie.set('color-theme', nextTheme, {
+    Cookie.set("color-theme", nextTheme, {
       expires: 1000,
     });
 
     // Apply the new colors to the root HTML tag.
-    const colors =
-      nextTheme === 'light'
-        ? LIGHT_TOKENS
-        : DARK_TOKENS;
+    const colors = nextTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
 
     const root = document.documentElement;
 
-    root.setAttribute(
-      'data-color-theme',
-      nextTheme
-    );
+    root.setAttribute("data-color-theme", nextTheme);
 
     Object.entries(colors).forEach(([key, value]) => {
       root.style.setProperty(key, value);
@@ -39,21 +33,11 @@ function DarkLightToggle({ initialTheme, className }) {
   }
 
   return (
-    <button
-      className={className}
-      onClick={handleClick}
-    >
-      {theme === 'light' ? (
-        <Sun size="1.5rem" />
-      ) : (
-        <Moon size="1.5rem" />
-      )}
-      <VisuallyHidden>
-        Toggle dark / light mode
-      </VisuallyHidden>
+    <button className={className} onClick={handleClick}>
+      {theme === "light" ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+      <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
     </button>
   );
 }
-
 
 export default DarkLightToggle;
